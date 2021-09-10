f57c1061bfd54c1e4427d23e0751c330a77a1037
Sanghak-Lee <eaa0305@naver.com>
2021-09-10 03:36:43 +0000

    docker-compose_ubuntu

---
+++ djecommerce/venv-python3/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\성남\pycharmprojects\ecommerce\venv-python3.7\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
